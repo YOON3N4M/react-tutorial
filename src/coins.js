00ae4f9a4f9fa9af8b6c5205272bd74d1fa0dcb6
yoon3nam <lgmysn2@gmail.com>
2022-11-19 19:51:11 +0900

    api 값 불러와서 넣기 전

--- src/coins.js
+++ src/coins.js
@@ -4,7 +4,8 @@ import "./coins.css";
 function Coins() {
   const [loading, setLoading] = useState(true);
   const [coins, setCoins] = useState([]);
-  const [money, setMoney] = useState();
+  const [money, setMoney] = useState(0);
+  const [disable, setDisable] = useState(false);
 
   useEffect(() => {
     fetch("https://api.coinpaprika.com/v1/tickers")
@@ -14,17 +15,132 @@ function Coins() {
         setLoading(false);
       }, []);
   });
+  const reset = () => setMoney(0);
+  const onChange = (event) => {
+    setMoney(() => event.target.value);
+  };
+  const invert = () => {
+    setDisable((prev) => !prev);
+    reset();
+  };
+  //
+  const setEmpty = () => {
+    if (money === 0) {
+      setMoney("");
+    }
+  };
+
+  const demical = money / 1300;
+  const fixed = demical.toFixed(2);
 
   return (
     <div id="coins">
       <div id="funds">
         <label>i have </label>
-        <input id="dollar" placeholder="enter your money($)"></input>
+        <input
+          disabled={disable}
+          value={disable ? fixed : money}
+          onChange={onChange}
+          id="dollar"
+          placeholder="enter your money($)"
+          type="number"
+          onClick={setEmpty}
+        ></input>
         <label> dollars</label>
         <br />
         <label>as KRW </label>
-        <input placeholder="enter your money(₩)" id="krw"></input>
+        <input
+          disabled={!disable}
+          onChange={onChange}
+          value={disable ? money : money * 1300}
+          //1달러 = 1300원으로 가정
+          placeholder="enter your money(₩)"
+          id="krw"
+          type="number"
+          onClick={setEmpty}
+        ></input>
         <label> won</label>
+        <br></br>
+        <button onClick={invert}>$ ↔ ₩</button>
+      </div>
+      <div id="converterHead">
+        <span>so i can buy....</span>
+      </div>
+      <div id="converterBox">
+        <div id="tableBox">
+          <table id="convertTable">
+            <tbody>
+              <tr>
+                <td>
+                  <p>
+                    <em>갯수</em>
+                    <em>태그</em>
+                  </p>
+                </td>
+                <td>
+                  {" "}
+                  <p>
+                    <em>갯수</em>
+                    <em>태그</em>
+                  </p>
+                </td>
+                <td>
+                  {" "}
+                  <p>
+                    <em>갯수</em>
+                    <em>태그</em>
+                  </p>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  {" "}
+                  <p>
+                    <em>갯수</em>
+                    <em>태그</em>
+                  </p>
+                </td>
+                <td>
+                  {" "}
+                  <p>
+                    <em>갯수</em>
+                    <em>태그</em>
+                  </p>
+                </td>
+                <td>
+                  {" "}
+                  <p>
+                    <em>갯수</em>
+                    <em>태그</em>
+                  </p>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  {" "}
+                  <p>
+                    <em>갯수</em>
+                    <em>태그</em>
+                  </p>
+                </td>
+                <td>
+                  {" "}
+                  <p>
+                    <em>갯수</em>
+                    <em>태그</em>
+                  </p>
+                </td>
+                <td>
+                  {" "}
+                  <p>
+                    <em>갯수</em>
+                    <em>태그</em>
+                  </p>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
       <div id="coinHead">
         <h1>Coins{loading ? "" : `(${coins.length})`}</h1>
@@ -36,7 +152,7 @@ function Coins() {
           <select style={{ backgroundColor: "#333651" }}>
             <option>choose coin</option>
             {coins.map((coin) => (
-              <option style={{ margin: 5 }} key={coin.id}>
+              <option style={{ margin: 5 }} key={coin.rank}>
                 {coin.name} ({coin.symbol}) : {coin.quotes.USD.price} (USD) /{" "}
                 {coin.quotes.USD.price * 1340.24} (KRW)
               </option>
